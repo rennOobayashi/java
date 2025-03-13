55a48b0f0ac129b7ba10593ee6375753578010f4
inhee <devforplayer@gmail.com>
2025-03-13 19:02:55 +0900

    v1.0 week2

---
+++ 1/Hello.java
@@ -0,0 +1,7 @@
+//program name: Hello.java
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+	}
+}
---
+++ 1/week1/src/week1/HelloWorld.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("학과: 컴퓨터정보공학과");
+		System.out.println("학번: 202444086");
+		System.out.println("이름: 이인희");
+	}
+}
