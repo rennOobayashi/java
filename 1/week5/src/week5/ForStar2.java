c2f2bbafa2a7a1b41b2e2734c068360c3cd23ac9
inhee <devforplayer@gmail.com>
2025-04-03 19:16:55 +0900

    v3.10 Oblong star

---
+++ 1/week5/src/week5/ForStar2.java
@@ -0,0 +1,12 @@
+package week5;
+
+public class ForStar2 {
+	public static void main(String[] args) {
+		for (int x = 0; x < 3; ++x) {
+			for (int y = 0; y < 5; ++y) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
