eb9d5cc1e3cf9e24d3f5dec72132ad63da52dd43
inhee <devforplayer@gmail.com>
2025-05-08 18:03:27 +0900

    v6.1 Getter, Setter

--- week10/src/week10/Car.java
+++ week10/src/week10/Car.java
@@ -15,4 +15,28 @@ public class Car {
 	public void setCompany(String _company) {
 		company = _company;
 	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
 }
